bb2ea433f10c636e48ff8c1abbd94eb4078e4b4e
jordan <jordan@bitola.com>
2022-05-30 10:53:17 -0400

    Enpty tree creation (with keys)

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { InputGroup, Button, FormControl, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'; 
 import Content from './Content';
+import { randomBytes } from 'crypto';
+import crypto from 'crypto';
 
 function Body({provider, address, ecdh}) {
     const [rootCid, setRootCid] = React.useState(null);
@@ -12,8 +14,39 @@ function Body({provider, address, ecdh}) {
 
     const onRootCidChange = (e) => { setRootCid(e.currentTarget.value); }
 
-    const onCreate = () => {
-        
+    const onCreate = async () => {
+        // Create dummy ECDH
+        const dummyECDH = crypto.createECDH('secp256k1');
+        dummyECDH.generateKeys();
+    
+        const keyEncryprionKey = ecdh.computeSecret(dummyECDH.getPublicKey());
+console.log("keyEncryprionKey: ", keyEncryprionKey.toString('hex').length, keyEncryprionKey.toString('hex'));
+        // Generate Common Secret for symmetric encryption of the content
+        const commonSecret = crypto.randomBytes(32);
+        const cipher = 'aes-256-ctr';
+        var crypter = crypto.createCipher(cipher, keyEncryprionKey);
+        let encCommonKey = Buffer.concat([crypter.update(commonSecret), crypter.final()]);
+console.log("CS: ", commonSecret.toString('hex').length, commonSecret.toString('hex'));
+console.log("Enc common key: ", encCommonKey.toString('hex').length, encCommonKey.toString('hex'));
+const d = crypto.createDecipher(cipher, keyEncryprionKey);
+const dcs = Buffer.concat([d.update(encCommonKey), d.final()]);
+console.log("DCS:", dcs.toString('hex').length, dcs.toString('hex'));
+
+console.log("peer:pubkey: ", dummyECDH.getPublicKey().toString('hex').length, dummyECDH.getPublicKey().toString('hex'));
+        const emptyTree = { 
+            Keys: [ { 
+                addr: address,
+                alias: "", 
+                key: { peer_pubkey: dummyECDH.getPublicKey().toString('hex'), enc_common_key: encCommonKey.toString('hex') }}], 
+            Content: "empty"
+        }
+console.log("emptyTree: ", emptyTree)
+
+        const cid = await window.ipfs.dag.put(emptyTree);
+
+        // No pinning planned, but this is the place to pin the rootCid
+
+        setRootCid(cid);
     }
 
     const refreshRequest = () => {
