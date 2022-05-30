c15e336dc851d2414a8f17049c2430b93ca28724
jordan <jordan@bitola.com>
2022-05-30 13:08:24 -0400

    User list controls

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,29 @@
 // SPDX-License-Identifier: BUSL-1.1
 import React from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'; 
 
 function UserList({provider, address, ecdh, rootCid}) {
+    const [myAlias, setMyAlias] = React.useState("");
+    const [inputAccessRequest, setInputAccessRequest] = React.useState(null);
+
+
     return (<>
+        <br/>
+        <Button>Request Access</Button>
+        <br/><br/>
+        <InputGroup className="mb-3">
+            <InputGroup.Text>Request</InputGroup.Text>
+            <FormControl placeholder="paste request here" value={inputAccessRequest} onChange={e => setInputAccessRequest(e.currentTarget.value)} />
+            <Button>Add user</Button>
+        </InputGroup>
+        <br/><br/>
+        <InputGroup className="mb-3">
+            <InputGroup.Text>My alias</InputGroup.Text>
+            <FormControl placeholder="type alias here" value={myAlias} onChange={e => setMyAlias(e.currentTarget.value)} />
+            <Button>Update</Button>
+        </InputGroup>
+
         UserList
     </>);
 }
